b29eb196e73791d40e312daebd33ca6df5140108
Dvir Dafrush <dvirdaf1@gmail.com>
2023-11-22 01:31:22 +0200

    fixed HHI tester, now working and well committed

--- source_files/rule testers/HHItester.py
+++ source_files/rule testers/HHItester.py
@@ -2,22 +2,31 @@ import requests
 
 #in order for this to work youll need this line in the hosts file:
 #127.0.0.1 testwaf202.com
-ans = requests.get("http://testwaf202.com/")
-print("ans: " + str(ans.status_code) + " | content: " +str(ans.content))
+ans = requests.get("http://testwaf202.com")
+print("should work: " + str(ans.status_code) + " | content: " +str(ans.content))
 input()
 
+#normal request but with manual host header that contains the http scheme
+headers = {'Host': 'http://testwaf202.com',}
+ans = requests.get("http://127.0.0.1", headers=headers)
+print("should work: " + str(ans.status_code) + " | content: " +str(ans.content))
+input()
+
+#bad request that dont have the correct host header
 ans = requests.get("http://127.0.0.1")
-print("ans: " + str(ans.status_code) + " | content: " +str(ans.content))
+print("should not work: " + str(ans.status_code) + " | content: " +str(ans.content))
 input()
 
 headers = {'Host': '\\n',}
-ans = requests.get("http://testwaf202.com/", headers=headers)
-print("ans: " + str(ans.status_code) + " | content: " +str(ans.content))
+ans = requests.get("http://testwaf202.com", headers=headers)
+print("should not work: " + str(ans.status_code) + " | content: " +str(ans.content))
 input()
 
-headers = {'Host': 'http://testwaf202.com/',
-           'X-Host': 'somthing'}
-ans = requests.get("http://testwaf202.com/", headers=headers)
-print("ans: " + str(ans.status_code) + " | content: " +str(ans.content))
+headers = {'Host': '',}
+ans = requests.get("http://testwaf202.com", headers=headers)
+print("should not work: " + str(ans.status_code) + " | content: " +str(ans.content))
 input()
 
+headers = {'Host': 'http://testwaf202.com', 'X-Host': 'somthing'}
+ans = requests.get("http://testwaf202.com", headers=headers)
+print("should not work: " + str(ans.status_code) + " | content: " +str(ans.content))
