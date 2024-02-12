afbaa1af922ed58949d96052d565ff631f41cc98
Or Sela <orselasha@gmail.com>
2024-02-12 13:46:28 +0200

    Fetch(manager panel):add rules design

--- source_files/WoofSourceFiles/WoofManagerPanel/ManagerPanel.py
+++ source_files/WoofSourceFiles/WoofManagerPanel/ManagerPanel.py
@@ -6,6 +6,9 @@ from typing import Annotated
 import fastapi.responses
 from uvicorn import run
 import os
+import sys
+from os import path
+import json
 # Import custom modules
 import serverInfoWrite
 
@@ -16,9 +19,25 @@ program_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "pages")
 
 templates = Jinja2Templates(directory=os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates"))
 
-
+json_path = os.path.join("..", 'server_properties.json')
 # Create the FastAPI instance
 app = FastAPI()
+
+
+def update_server_json(new_data):
+    with open(json_path, 'r') as f:
+        existing_data = json.load(f)
+
+    # Update rules based on received data
+    existing_data['rules'] = {
+        key: bool(new_data.get(key))  # Use `.get` to handle missing keys
+        for key in existing_data['rules']
+    }
+
+    # Write updated data to the file
+    with open(json_path, 'w') as f:
+        json.dump(existing_data, f, indent=4)
+    return existing_data
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -47,11 +66,33 @@ async def root(request: Request, hostname: Annotated[str, Form()] = None,
 
 @app.post('/submit-rules')
 async def root(request: Request):
+    # Access form data and update server.json
     form_data = await request.form()
-    ddos_enabled = form_data.get("DDOS") == "on"  # True if enabled, False otherwise
-    host_header_injection_enabled = form_data.get("HostHeaderInjection") == "on"
-    # Access other options similarly
-    return "ok ok"
+    update_server_json(form_data)
+
+    # Load current rules from server.json
+    with open(json_path, 'r') as f:
+        existing_data = json.load(f)
+        rules = existing_data['rules']
+
+    # Redirect with rules data as query parameter
+    rules_string = json.dumps(rules)
+    url = "/pages/rules.html"  # ?rules=" + rules_string
+    return RedirectResponse(url=url, status_code=302)
+
+
+@app.get("/check-switches")
+async def check_switches():
+    try:
+        with open(json_path, "r") as f:
+            rules_data = json.load(f)
+
+        rules_dict = {key: bool(value) for key, value in rules_data["rules"].items()}  # Convert strings to booleans
+
+        return json.dumps(rules_dict)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error fetching switch states: {e}")
 
 
 @app.get("/favicon.ico")
@@ -61,6 +102,7 @@ async def root():
         return fastapi.responses.FileResponse(favicon_path)
     print("[!] cant find favicon")
     return None
+
 
 # Serve static files from the "pages" directory
 app.mount("/pages", StaticFiles(directory=program_dir), name="pages")
