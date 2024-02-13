1562532148601bd978fb35c4fd707376a35f883d
Or Sela <orselasha@gmail.com>
2024-02-13 20:07:15 +0200

    Fetch(setting page): finish settings page

--- source_files/WoofSourceFiles/WoofManagerPanel/ManagerPanel.py
+++ source_files/WoofSourceFiles/WoofManagerPanel/ManagerPanel.py
@@ -25,7 +25,7 @@ json_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'serv
 app = FastAPI()
 
 
-def update_server_json(new_data):
+def update_server_rules_json(new_data):
     with open(json_path, 'r') as f:
         existing_data = json.load(f)
 
@@ -41,6 +41,20 @@ def update_server_json(new_data):
     return existing_data
 
 
+def update_server_setting_json(new_data):
+    with open(json_path, 'r') as f:
+        existing_data = json.load(f)
+
+    for key in existing_data:
+        if new_data.get(key):
+            existing_data[key] = new_data.get(key)
+    existing_data["port"] = int(existing_data["port"])
+    # Write updated data to the file
+    with open(json_path, 'w') as f:
+        json.dump(existing_data, f, indent=4)
+    return existing_data
+
+
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     # Use RedirectResponse to automatically redirect to /pages/menu.html
@@ -48,28 +62,30 @@ async def root(request: Request):
 
 
 @app.post("/submit-settings")
-async def root(request: Request, hostname: Annotated[str, Form()] = None,
-               ip: Annotated[str, Form()] = None, port: Annotated[int, Form()] = None):
-    new_settings = dict()
+async def root(request: Request):
+    # Access form data and update server.json
+    form_data = await request.form()
+    with open(json_path, 'r') as f:
+        existing_data = json.load(f)
 
-    if hostname:
-        new_settings['host_name'] = hostname
-    if ip:
-        new_settings['ip'] = ip
-    if port:
-        new_settings['port'] = port
+    new_settings = {
+        "server_host": existing_data["server_host"],
+        "ip": existing_data["ip"],
+        "port": existing_data["port"],
+    }
+    update_server_setting_json(form_data)
 
-    serverInfoWrite.set_json_argument(new_settings)
-    print(new_settings)
-    # Return a redirect to home page or somthing
-    return 'submitted'
+    # Redirect with settings data as query parameters
+    settings_string = json.dumps(new_settings)
+    url = f"/pages/settings.html?settings={settings_string}"
+    return RedirectResponse(url=url, status_code=302)
 
 
 @app.post('/submit-rules')
 async def root(request: Request):
     # Access form data and update server.json
     form_data = await request.form()
-    update_server_json(form_data)
+    update_server_rules_json(form_data)
 
     # Load current rules from server.json
     with open(json_path, 'r') as f:
@@ -93,6 +109,29 @@ async def check_switches():
         # Redirect with rules data as query parameter
         rules_string = json.dumps(rules)
         url = "/pages/rules.html?rules=" + rules_string
+        print(url)
+        return JSONResponse(content={"url": url}, status_code=302)
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=f"Error fetching switch states: {e}")
+
+
+@app.get("/check-settings")
+async def check_settings():
+    try:
+        with open(json_path, 'r') as f:
+            existing_data = json.load(f)
+
+        new_settings = {
+            "server_host": existing_data["server_host"],
+            "ip": existing_data["ip"],
+            "port": existing_data["port"],
+        }
+
+        # Redirect with settings data as query parameters
+        settings_string = json.dumps(new_settings)
+        url = f"/pages/settings.html?settings={settings_string}"
         print(url)
         return JSONResponse(content={"url": url}, status_code=302)
 
--- source_files/WoofSourceFiles/WoofManagerPanel/serverInfoWrite.py
+++ source_files/WoofSourceFiles/WoofManagerPanel/serverInfoWrite.py
@@ -7,22 +7,25 @@ from typing import Dict, Any
 current_dir = os.path.dirname(os.path.realpath(__file__))
 server_properties_dir = os.path.join(current_dir, '..')
 
+
 def set_json_argument(new_args_dict: Dict[str, Any]):
     """Writes a key-value pair to the server_properties.json file, ensuring file integrity and consistency.
 
     Args:
         key (str): The key to be updated.
         value: The new value to be assigned to the key.
+        @param new_args_dict:
     """
 
-    with filelock.FileLock(os.path.join(server_properties_dir, 'server_properties.json.lock')):  # Acquire a lock to prevent race conditions
-        with open(os.path.join(server_properties_dir, 'server_properties.json'), 'r+') as f:  # Open file for reading and writing
+    with filelock.FileLock(os.path.join(server_properties_dir,
+                                        'server_properties.json.lock')):  # Acquire a lock to prevent race conditions
+        with open(os.path.join(server_properties_dir, 'server_properties.json'),
+                  'r+') as f:  # Open file for reading and writing
             try:
                 data = json.load(f)  # Load existing JSON data
 
                 for key in new_args_dict:
                     data[key] = new_args_dict[key]  # Update the value for the specified key
-
 
                 f.seek(0)  # Move the file pointer to the beginning
                 json.dump(data, f, indent=4)  # Write the updated data back to the file in a readable format
