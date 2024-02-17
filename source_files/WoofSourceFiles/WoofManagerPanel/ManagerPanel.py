7c5834660d26cd02a1c9d76603cc33cc254f9f8c
Dvir Dafrush <dvirdaf1@gmail.com>
2024-02-18 01:43:38 +0200

    fixed request handling for rules and settings

--- source_files/WoofSourceFiles/WoofManagerPanel/ManagerPanel.py
+++ source_files/WoofSourceFiles/WoofManagerPanel/ManagerPanel.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request, Form, Response, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, RedirectResponse
 from fastapi.responses import RedirectResponse, FileResponse, HTMLResponse
 import fastapi.responses
 from uvicorn import run
@@ -13,86 +13,33 @@ import serverInfoWrite
 # check about middleware for blocking no logged users
 
 # Get program dir for file usage
-program_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "pages")
+program_dir = os.path.dirname(os.path.abspath(__file__))
+pages_dir = os.path.join(program_dir, "pages")
 
-templates = Jinja2Templates(directory=os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates"))
+# templates = Jinja2Templates(directory=os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates"))
 
-json_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'server_properties.json')
+json_path = os.path.join(program_dir, '..', 'server_properties.json')
+
 # Create the FastAPI instance
 app = FastAPI()
 
 
-def update_server_rules_json(new_data):
-    with open(json_path, 'r') as f:
-        existing_data = json.load(f)
-
-    # Update rules based on received data
-    existing_data['rules'] = {
-        key: new_data.get(key) == "on"  # Use `.get` to handle missing keys
-        for key in existing_data['rules']
-    }
-
-    # Write updated data to the file
-    with open(json_path, 'w') as f:
-        json.dump(existing_data, f, indent=4)
-    return existing_data
-
-
-def update_server_setting_json(new_data):
-    with open(json_path, 'r') as f:
-        existing_data = json.load(f)
-
-    for key in existing_data:
-        if new_data.get(key):
-            existing_data[key] = new_data.get(key)
-    existing_data["port"] = int(existing_data["port"])
-    # Write updated data to the file
-    with open(json_path, 'w') as f:
-        json.dump(existing_data, f, indent=4)
-    return existing_data
-
-
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    # Use RedirectResponse to automatically redirect to /pages/menu.html
-    return RedirectResponse(url="/pages/menu.html", status_code=302)
+    # Redirect to /pages/menu.html
+    return RedirectResponse(url='/pages/menu.html')
 
 
 @app.post("/submit-settings")
 async def root(request: Request):
-    # Access form data and update server.json
-    form_data = await request.form()
-    with open(json_path, 'r') as f:
-        existing_data = json.load(f)
-
-    new_settings = {
-        "server_host": existing_data["server_host"],
-        "ip": existing_data["ip"],
-        "port": existing_data["port"],
-    }
-    update_server_setting_json(form_data)
-
-    # Redirect with settings data as query parameters
-    settings_string = json.dumps(new_settings)
-    url = f"/pages/settings.html?settings={settings_string}"
-    return RedirectResponse(url=url, status_code=302)
+    form_data = await request.json()
+    serverInfoWrite.set_settings_json_argument(form_data)
 
 
 @app.post('/submit-rules')
 async def root(request: Request):
-    # Access form data and update server.json
-    form_data = await request.form()
-    update_server_rules_json(form_data)
-
-    # Load current rules from server.json
-    with open(json_path, 'r') as f:
-        existing_data = json.load(f)
-        rules = existing_data['rules']
-
-    # Redirect with rules data as query parameter
-    rules_string = json.dumps(rules)
-    url = "/pages/rules.html?rules=" + rules_string
-    return RedirectResponse(url=url, status_code=302)
+    form_data = await request.json()
+    serverInfoWrite.set_rules_json_argument(form_data)
 
 
 @app.get("/check-switches")
@@ -105,9 +52,8 @@ async def check_switches():
 
         # Redirect with rules data as query parameter
         rules_string = json.dumps(rules)
-        url = "/pages/rules.html?rules=" + rules_string
-        print(url)
-        return JSONResponse(content={"url": url}, status_code=302)
+        # url = "/pages/rules.html?rules=" + rules_string
+        return JSONResponse(content=rules)
 
     except Exception as e:
         print(e)
@@ -120,17 +66,8 @@ async def check_settings():
         with open(json_path, 'r') as f:
             existing_data = json.load(f)
 
-        new_settings = {
-            "server_host": existing_data["server_host"],
-            "ip": existing_data["ip"],
-            "port": existing_data["port"],
-        }
-
-        # Redirect with settings data as query parameters
-        settings_string = json.dumps(new_settings)
-        url = f"/pages/settings.html?settings={settings_string}"
-        print(url)
-        return JSONResponse(content={"url": url}, status_code=302)
+        del existing_data["rules"]
+        return JSONResponse(content=existing_data, status_code=302)
 
     except Exception as e:
         print(e)
@@ -147,7 +84,7 @@ async def root():
 
 
 # Serve static files from the "pages" directory
-app.mount("/pages", StaticFiles(directory=program_dir), name="pages")
+app.mount("/pages", StaticFiles(directory=pages_dir), name="pages")
 
 #main function
 if __name__ == '__main__':
