e055d6f206eacb454b1bca4d297ed7896d9714f2
Dvir Dafrush <dvirdaf1@gmail.com>
2024-02-18 01:43:01 +0200

    made a main page

---
+++ source_files/WoofSourceFiles/WoofManagerPanel/pages/main.js
@@ -0,0 +1,24 @@
+function loadMenu() {
+  const settingsFrame = document.getElementById("contentFrame");
+  settingsFrame.src = "/pages/menu.html";
+}
+function loadSettings() {
+  const settingsFrame = document.getElementById("contentFrame");
+  settingsFrame.src = "/pages/settings.html";
+}
+function loadLogs() {
+  const settingsFrame = document.getElementById("contentFrame");
+  settingsFrame.src = "/pages/logs.html";
+}
+function loadSecLogs() {
+  const settingsFrame = document.getElementById("contentFrame");
+  settingsFrame.src = "/pages/security_logs.html";
+}
+function loadRules() {
+  const settingsFrame = document.getElementById("contentFrame");
+  settingsFrame.src = "/pages/rules.html";
+}
+function loadRestrictUser() {
+  const settingsFrame = document.getElementById("contentFrame");
+  settingsFrame.src = "/pages/restrict_user.html";
+}
