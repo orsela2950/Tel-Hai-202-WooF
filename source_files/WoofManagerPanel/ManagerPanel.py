645e199d44512cdfa2667a15c5cba67512b3ba91
Or Sela <orselasha@gmail.com>
2024-02-26 19:12:38 +0200

    Fetch(blacklist page): add page the show the black list and let user delete from black list

--- source_files/WoofManagerPanel/ManagerPanel.py
+++ source_files/WoofManagerPanel/ManagerPanel.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from uvicorn import run
 from os.path import join, abspath, dirname, isfile
 import json
+import sqlite3
 # Import custom modules
 import serverInfoWrite
 
@@ -14,6 +15,7 @@ PORT = 20343
 program_dir = abspath(dirname(__file__))
 pages_dir = join(program_dir, 'pages')
 json_path = join(program_dir, '..', 'WoofSourceFiles/server_properties.json')
+db_path = join(program_dir, '..', 'WoofSourceFiles/punishment_db.db')
 logs_dir = join(program_dir, '..', 'logs')
 logs_path = join(logs_dir, 'main.toml')
 security_logs_path = join(logs_dir, 'security.toml')
@@ -77,6 +79,56 @@ async def root():
 @app.get('/security_logs')
 async def root():
     return FileResponse(security_logs_path)
+
+
+@app.get('/blacklist')
+async def get_blacklist():
+    try:
+        # Connect to the SQLite database
+        with sqlite3.connect(db_path) as conn:
+            cursor = conn.cursor()
+
+            # Fetch all rows from the blacklist table
+            cursor.execute("SELECT * FROM blacklist")
+            rows = cursor.fetchall()
+
+            # Convert rows to a list of dictionaries
+            blacklist_data = []
+            for row in rows:
+                entry = {
+                    'id': row[0],
+                    'ip_address': row[1],
+                    'reason': row[2],
+                    'expiration_date': row[3],
+                    'source': row[4]
+                }
+                blacklist_data.append(entry)
+
+            return JSONResponse(content={'blacklist': blacklist_data})
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=f'Error fetching blacklist data: {e}')
+
+
+# Add a new endpoint to handle row deletion
+@app.delete('/delete/{row_id}')
+async def delete_row(row_id: int):
+    try:
+        with sqlite3.connect(db_path) as conn:
+            cursor = conn.cursor()
+
+            # Delete the row with the specified ID from the blacklist table
+            cursor.execute("DELETE FROM blacklist WHERE id = ?", (row_id,))
+            conn.commit()
+            cursor.execute("DELETE FROM strikes WHERE id = ?", (row_id,))
+            conn.commit()
+
+        return JSONResponse(content={'message': 'Row deleted successfully'})
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=f'Error deleting row: {e}')
 
 
 @app.get('/favicon.ico')
